1738bc5e93e47f1df7552edae4860d48f3774aee
pdendy <68658567+dendypat@users.noreply.github.com>
2022-12-05 21:42:37 -0500

    "add libraries"

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import config from './aws-exports';
 
 Amplify.configure(config);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 
 
